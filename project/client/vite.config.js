7e3520a5489e34cde70ace4a041a972f00f2e517
Fabricio Laboriel <fabriciolaboriel@gmail.com>
2025-02-17 15:20:55 -0700

    added uri

--- project/client/vite.config.js
+++ project/client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: process.env.VITE_GRAPHQL_URI || 'http://localhost:3001',
         secure: false,
         changeOrigin: true
       }
